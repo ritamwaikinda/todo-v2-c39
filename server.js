ab0c85f61ef662efdb772b091640dff09630439f
Rita Mwaikinda <srmwaikinda@gmail.com>
2020-10-16 12:29:09 -0400

    configured to post to localhost

--- server.js
+++ server.js
@@ -17,18 +17,23 @@ const port = process.env.PORT || "8000";
 /**
  *  App Configuration
  */
-
+app.use(express.json())
 /**
  * Routes Definitions
  */
 const items = [];
 let id = 0;
 
-app.get('api/items', (req, res, next) => {
+
+app.get("/", (req, res) => {
+    res.status(200).send("TO DO App");
+});
+
+app.get('/api/items', (req, res, next) => {
     res.json(items);
 });
 
-app.post('api/items', (req, res, next) => {
+app.post('/api/items', (req, res, next) => {
 	// something goes in here
   console.log('req.body', req.body);
   if (req.body.item) {
