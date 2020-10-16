1fe65bd3e709186c183b6947ae3811156d6708c1
Rita Mwaikinda <srmwaikinda@gmail.com>
2020-10-16 12:02:05 -0400

    app get, post updated

--- server.js
+++ server.js
@@ -21,10 +21,31 @@ const port = process.env.PORT || "8000";
 /**
  * Routes Definitions
  */
+const items = [];
+let id = 0;
 
-app.get("/", (req, res) => {
-    res.status(200).send("TO DO App");
+app.get('api/items', (req, res, next) => {
+    res.json(items);
 });
+
+app.post('api/items', (req, res, next) => {
+	// something goes in here
+  console.log('req.body', req.body);
+  if (req.body.item) {
+	  id = id + 1;
+    const newItem = {
+	    id: id,
+	    item: req.body.item,
+      completed: false
+    };
+    items.push(newItem);
+    res.json(newItem);
+  } else {
+    res.status(400).json({error: "item needs a description"});
+  }
+});
+
+
 /**
  * Server Activation
  */
