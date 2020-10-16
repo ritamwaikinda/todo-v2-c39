442a09e9a7a569d29b6e7e9e705c8933ebabd5bb
Rita Mwaikinda <srmwaikinda@gmail.com>
2020-10-16 11:04:31 -0400

    Initing

---
+++ server.js
@@ -0,0 +1,34 @@
+// server.js
+
+/**
+ * Required External Modules
+ */
+
+const express = require ("express");
+const path = require ("path");
+
+/**
+ * App Variables
+ */
+
+const app = express();
+const port = process.env.PORT || "8000";
+
+/**
+ *  App Configuration
+ */
+
+/**
+ * Routes Definitions
+ */
+
+app.get("/", (req, res) => {
+    res.status(200).send("TO DO App");
+});
+/**
+ * Server Activation
+ */
+
+ app.listen(port, () => {
+     console.log(`Listening to requests on http://localhost:${port}`);
+ })
